e72ab9b822d56e68c5fd340926fa6ac9caf79351
Tim Horist Jr <Tim Horist Jr>
2016-01-29 11:42:48 -0800

    Reordered css selectors by section; added descriptive comments; added new card flip trigger in script.js (not yet implemented) in prep for v0.5

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
-$(document).ready(function(){
+function hideCardBackIfInvisible(cardBack) {
+    if (cardBack.css('opacity') == 0) {
+        cardBack.hide();
+        var card = cardBack.parent();
+        flipCard(card);
+    }
+}
 
+function flipCard(card){
+
+}
+
+$(document).ready(function(){
+    $('.card,.card img').attr({'draggable': 'false'});
+
+    $('.card').on('mouseup','.back',function(){
+        hideCardBackIfInvisible($(this));
+    });
+    $('.card').on('mouseleave','.back',function(){
+        hideCardBackIfInvisible($(this));
+    });
 });
